2b93f011f8588936d6d96c3349a793c5c5586404
sam <sam.d.arrowsmith@gmail.com>
2024-06-17 14:27:40 +0100

    Initial Commit

---
+++ src/main/java/com/raincheck/RainCheck/RainCheckApplication.java
@@ -0,0 +1,13 @@
+package com.raincheck.RainCheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainCheckApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainCheckApplication.class, args);
+	}
+
+}
